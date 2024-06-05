1b007b23aec30b84ac62009ea819ccbce721b1ca
Robin Green <robin@designingreen.co.uk>
2024-06-05 18:10:51 +0100

    mobile edtis

--- template-parts/home-origin.php
+++ template-parts/home-origin.php
@@ -4,10 +4,10 @@
 			<div class="col-12">
 				<h3><?php the_field('origin_title','option');?></h3>
 			</div>
-			<div class="col-12 col-md-6" style="padding-right:120px;">
+			<div class="col-12 col-md-5">
 				<p class="bold"><?php the_field('origin_introduction','option');?></p>
 			</div>
-			<div class="col-12 col-md-6" style="padding-right:120px;">
+			<div class="col-12 col-md-5 offset-md-1">
 				<?php the_field('origin_bodycopy','option');?>
 			</div>
 		</div>
