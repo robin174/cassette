02aed40b2eafab47391af759ac55db117bcefc8f
Robin Green <robin@designingreen.co.uk>
2024-06-12 19:20:43 +0100

    Tweak to homepage website

--- template-parts/home-origin.php
+++ template-parts/home-origin.php
@@ -1,14 +1,18 @@
 <section class="org--content-origin">
-	<div class="container">
-		<div class="row ">
-			<div class="col-12">
-				<h3><?php the_field('origin_title','option');?></h3>
-			</div>
-			<div class="col-12 col-md-5">
-				<p class="bold"><?php the_field('origin_introduction','option');?></p>
-			</div>
-			<div class="col-12 col-md-5 offset-md-1">
-				<?php the_field('origin_bodycopy','option');?>
+	<div class="container-fluid">
+		<div class="row justify-content-center">
+			<div class="col-12 col-md-10 col-xl-8 justify-content-center">
+				<div class="row">
+					<div class="col-12">
+						<h3><?php the_field('origin_title','option');?></h3>
+					</div>
+					<div class="col-12 col-lg-5">
+						<p class="bold"><?php the_field('origin_introduction','option');?></p>
+					</div>
+					<div class="col-12 col-lg-5 offset-lg-1">
+						<?php the_field('origin_bodycopy','option');?>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
--- template-parts/home-welcome.php
+++ template-parts/home-welcome.php
@@ -1,19 +1,21 @@
 <section class="org--content-welcome">
-	<div class="container">
-		<div class="row">
-			<div class="col-12 col-md-6">
+	<div class="org--content-welcome-inner">
+		<div class="container-fluid">
+			<div class="row justify-content-center">
+				<div class="col-12 col-md-10 col-xl-8 justify-content-center">
 
-				<section><!-- set height I guess or jus tcontent + padding -->
-					<!-- Introducing -->
-					<span class="introducing d-block"><?php the_field('intro_verb','option');?></span>
-					<!-- Logo -->
-					<img style="width: 840px;" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-cassette-dk.svg" alt="Cassette logo">
-					<!-- Description -->
-					<h2 style="font-size:1.8rem;line-height: 1.4;"><?php the_field('intro_description','option');?></</h2>
-					<!-- Call to action button-->
-					<p>Button</p>
-				</section>
+					<section><!-- set height I guess or jus tcontent + padding -->
+						<!-- Introducing -->
+						<span class="introducing d-block"><?php the_field('intro_verb','option');?></span>
+						<!-- Logo -->
+						<img class="mw-100" style="width: 840px;" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-cassette-dk.svg" alt="Cassette logo">
+						<!-- Description -->
+						<h2 style="font-size:1.8rem;line-height: 1.4;"><?php the_field('intro_description','option');?></</h2>
+						<!-- Call to action button-->
+						<p>Button</p>
+					</section>
 
+				</div>
 			</div>
 		</div>
 	</div>
