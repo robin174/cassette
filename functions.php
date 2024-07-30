484eddc0815b5b04e9474192499b6607761ea378
Robin Green <robin@designingreen.co.uk>
2024-07-30 15:34:23 +0100

    Add new section

--- functions.php
+++ functions.php
@@ -90,6 +90,11 @@ if( function_exists('acf_add_options_page') ) {
         'parent_slug'   => 'additional-site-content',
     ));
     acf_add_options_sub_page(array(
+        'page_title'    => 'Banner Advert',
+        'menu_title'    => 'Banner Advert',
+        'parent_slug'   => 'additional-site-content',
+    ));
+    acf_add_options_sub_page(array(
         'page_title'    => 'Footer Content',
         'menu_title'    => 'Footer Content',
         'parent_slug'   => 'additional-site-content',
